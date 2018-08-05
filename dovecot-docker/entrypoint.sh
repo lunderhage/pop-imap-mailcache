fe6b2c238195c367925ca224623cdadd02a5b9fb
Andreas Lunderhage <lunderhage@gmail.com>
2018-08-05 14:26:07 +0200

    Fixed dovecot SSL config and more.

--- dovecot-docker/entrypoint.sh
+++ dovecot-docker/entrypoint.sh
@@ -1,25 +1,46 @@
-#!/bin/sh
-
+#!/bin/bash
+set -e
 # We will want to generate keys on first start.
 
+DOVECOT_INITIALIZED=/etc/dovecot/dovecot.initialized
 USERS_FILE=/etc/dovecot/users
+DH_PEM=/etc/ssl/dovecot/dh.pem
+DOVECOT_SSL_CONFIG=/etc/dovecot/dovecot-openssl.cnf
+DOVECOT_SSL_PARAMETERS="IMAP_SSL_BITS IMAP_SSL_OU IMAP_SSL_CN"
+ALL_PARAMS="IMAP_USER IMAP_PASSWORD $DOVECOT_SSL_PARAMETERS"
+SSL_CERT=/etc/ssl/dovecot/server.pem
+SSL_KEY=/etc/ssl/dovecot/server.key
 
-if [ ! -s "$USERS_FILE" ]; then
+# Check variables
+for PARAMETER in $ALL_PARAMS; do
+	if [ -z "${!PARAMETER}" ]; then
+		echo $PARAMETER is not set.
+		exit 1
+	fi
+done		
+
+if [ ! -e $DOVECOT_INITIALIZED ]; then
+
+	cp /etc/dovecot/dovecot-openssl.cnf.template /etc/dovecot/dovecot-openssl.cnf
+	for PARAMETER in $DOVECOT_SSL_PARAMETERS; do
+		sed -i 's/${'${PARAMETER}'}/'"${!PARAMETER}"'/g' $DOVECOT_SSL_CONFIG
+	done
+
+	openssl gendh $IMAP_SSL_DH_BITS > /etc/ssl/dovecot/dh.pem
+	openssl req -new -x509 -nodes -config $DOVECOT_SSL_CONFIG -out $SSL_CERT -keyout $SSL_KEY -days 3650
+	chmod 0600 $SSL_KEY
+	openssl x509 -subject -fingerprint -noout -in $SSL_CERT
+	mkdir -p /usr/local/share/ca-certificates
+	ln -s $SSL_CERT /usr/local/share/ca-certificates/server.pem
+	update-ca-certificates
+
 	chown mailuser /mail -R
-	if [ -z "$USER" ]; then
-		echo "User must be set with USER variable."
-		exit 1
-	fi
 
-	if [ -z "$PASSWORD" ]; then
-		echo "Password must be set with PASSWORD variable."
-		exit 1
-	fi
-
-	BCRYPT_PASS=$(doveadm pw -s BLF-CRYPT -p $PASSWORD)
+	BCRYPT_PASS=$(doveadm pw -s BLF-CRYPT -p $IMAP_PASSWORD)
 	# echo "$USER:$BCRYPT_PASS:$MAIL_UID:$MAIL_GID::/mail/%u::userdb_mail=maildir:~/Maildir" > $USERS_FILE
-	echo "$USER:$BCRYPT_PASS:$MAIL_UID:$MAIL_GID" > $USERS_FILE
-fi
+	echo "$IMAP_USER:$BCRYPT_PASS:$MAIL_UID:$MAIL_GID" > $USERS_FILE
 
+	touch $DOVECOT_INITIALIZED
+fi
 
 /usr/sbin/dovecot -F
